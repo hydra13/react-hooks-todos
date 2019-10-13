96ad6fc8213599c825834e5c2aa82fabcb0c74a4
hydra13 <rob.sabirov@yandex.ru>
2019-10-13 21:04:41 +0300

    added switching between pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-import Todo from './components/Todo'
+import Todo from './components/Todo';
+import Header from './components/Header';
+import Auth from './components/Auth';
+
+const App = props => {
+    const [page, setPage] = useState('auth');
+
+    const switchPage = pageName => {
+        setPage(pageName);
+    }
+
+    return (
+        <div>
+            <Header 
+                onLoadTodos={ switchPage.bind(null, 'todos') }
+                onLoadAuth={ switchPage.bind(null, 'auth') }
+            />
+            <hr />
+            { page !== 'auth' ? <Todo /> : <Auth /> }
+        </div>
+    )
+}
 
 ReactDOM.render(
-    <Todo />,
+    <App />,
     document.getElementById('app')
 )
