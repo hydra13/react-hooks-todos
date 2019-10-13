08ab29fd729b1c08bbceaf48ff377e6b7ff83c8f
hydra13 <rob.sabirov@yandex.ru>
2019-10-13 21:18:59 +0300

    test useContext hook

--- src/App.js
+++ src/App.js
@@ -4,22 +4,30 @@ import ReactDOM from 'react-dom';
 import Todo from './components/Todo';
 import Header from './components/Header';
 import Auth from './components/Auth';
+import AuthContext from './auth-context';
 
 const App = props => {
     const [page, setPage] = useState('auth');
+    const [authStatus, setAuthStatus] = useState(false);
 
     const switchPage = pageName => {
         setPage(pageName);
     }
 
+    const login = () => {
+        setAuthStatus(true);
+    }
+
     return (
         <div>
-            <Header 
-                onLoadTodos={ switchPage.bind(null, 'todos') }
-                onLoadAuth={ switchPage.bind(null, 'auth') }
-            />
-            <hr />
-            { page !== 'auth' ? <Todo /> : <Auth /> }
+            <AuthContext.Provider value={{status: authStatus, login}}>
+                <Header 
+                    onLoadTodos={ switchPage.bind(null, 'todos') }
+                    onLoadAuth={ switchPage.bind(null, 'auth') }
+                />
+                <hr />
+                { page !== 'auth' ? <Todo /> : <Auth /> }
+            </AuthContext.Provider>
         </div>
     )
 }
